b10efcc918e30b9f6d162adfdda5f54f9d0942b5
Neil Hunter <neilhunter40@gmail.com>
2020-05-06 08:02:15 +0100

    Added Bootstrap apptempt 1
    - Added scripts to bundle
    - Added css to bundle

--- DonaldsonMotors/App_Start/BundleConfig.cs
+++ DonaldsonMotors/App_Start/BundleConfig.cs
@@ -9,7 +9,20 @@ namespace DonaldsonMotors
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/main.js",
+                        "~/vendor/jquery/jquery.min.js",
+                        "~/vendor/bootstrap/js/bootstrap.bundle.min.js",
+                        "~/vendor/jquery.easing/jquery.easing.min.js",
+                        "~/vendor/php-email-form/validate.js",
+                        "~/vendor/counterup/counterup.min.js",
+                        "~/vendor/tether/js/tether.min.js",
+                        "~/vendor/jquery-sticky/jquery.sticky.js",
+                        "~/vendor/venobox/venobox.min.js",
+                        "~/vendor/lockfixed/jquery.lockfixed.min.js",
+                        "~/vendor/waypoints/jquery.waypoints.min.js",
+                        "~/vendor/superfish/superfish.min.js",
+                        "~/vendor/hoverIntent/hoverIntent.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -24,7 +37,11 @@ namespace DonaldsonMotors
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/vendor/bootstrap/css/bootstrap.min.css",
+                      "~/vendor/venobox/venobox.css",
+                      "~/vendor/font-awesome/css/font-awesome.min.css",
+                      "~/Content/style.css"));
         }
     }
 }
