09c4e6475656227ff1c5764b5992affea72832cc
Neil Hunter <neilhunter40@gmail.com>
2020-05-10 14:44:57 +0100

    update

--- DonaldsonMotors/Controllers/BookingController.cs
+++ DonaldsonMotors/Controllers/BookingController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DonaldsonMotors.Models.SystemParts;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -13,10 +14,24 @@ namespace DonaldsonMotors.Controllers
         {
             return View();
         }
-
+        [HttpGet]
         public ActionResult CheckAvalability()
         {
             return View();
+        }
+
+        public ActionResult CheckAvalability(DateTime date)
+        {
+            List<Slot> slots = new List<Slot>();
+
+            return View(slots);
+        }
+
+        public List<Slot> GetSlotsList()
+        {
+            List<Slot> slots = new List<Slot>();
+
+            return slots;
         }
     }
 }
---
+++ DonaldsonMotors/Models/SystemParts/Slot.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DonaldsonMotors.Models.SystemParts
+{
+    public class Slot
+    {
+        public DateTime date { get; set; }
+        public int start { get; set; }
+        public int end { get; set; }
+        public string desc { get; set; }
+
+
+    }
+}
